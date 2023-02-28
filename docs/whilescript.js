00d0b89646b810ca2bade58c76dfcaf5246bd05f
Lisa Patacchiola <lpatacch@gmail.com>
2023-02-27 19:48:10 -0500

    while practice
    Quickly out together the while emulator. It can only check <, >, <=, and >=. Also, have - and + to change the numbers.
    Need to do more error checks and fix it, so it isn't off to the side.
    Need a better explanation of how to use it.
    Need more comparisons and operators.

---
+++ docs/whilescript.js
@@ -0,0 +1,79 @@
+
+
+// Selecting button element
+var btn = document.getElementById("check");
+
+ 
+// Assigning event listeners to the button
+btn.addEventListener("click", checkFunction);
+
+function validOp(val) {
+	if (val == "+")
+		return true;
+	if (val == "-")
+		return true;
+	return false;
+}
+
+function modify(num1, op, num2)
+{
+	if (op == "+")
+		return num1 + num2;
+	if (op == "-")
+		return num1 - num2;
+}
+
+function validCond(val) {
+	if (val == "<")
+		return true;
+	if (val == "<=")
+		return true;
+	if (val == ">")
+		return true;
+	if (val == ">=")
+		return true;
+	return false;
+}
+
+function useCond(num1, val, num2) {
+	if (val == "<")
+		return (num1 < num2)
+	if (val == ">")
+		return (num1 > num2)
+	if (val == ">=")
+		return (num1 >= num2)
+	if (val == "<=")
+		return (num1 <= num2)
+}
+	
+function checkFunction() {
+  let beg = document.getElementById("begval").value;
+  let end = document.getElementById("endval").value;
+  let change = document.getElementById("changeval").value;
+  let endc = document.getElementById("endcond").value;
+  let opp = document.getElementById("operator").value;
+  let hints = document.getElementById("hints");
+  let topVal = parseInt(end);
+  let incVal = parseInt(change);
+  
+  let printVal = "";
+  num = parseInt(beg);
+  if (!validCond(endc))
+  {
+	  hints.innerHTML = "Sorry, while condition is not valid";
+	  return;
+  }
+  if (!validOp(opp))
+  {
+	  hints.innerHTML = "Sorry, operator is not valid";
+	  return;
+  }
+  while (useCond(num, endc, topVal))
+  {
+	  printVal += num + " ";
+	  num = modify(num, opp, incVal);
+  }
+  printVal += "done";
+  hints.innerHTML = printVal;
+  
+}
